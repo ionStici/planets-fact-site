7d23e6bca0c058faf63b5d579d0f529f95e6405b
Ion Stici <ionsticidev@gmail.com>
2023-10-30 15:50:15 +0200

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,54 @@
 import Link from "next/link";
 import Image from "next/image";
+
+import { useState, useRef } from "react";
+
 import classes from "./header.module.scss";
 
 function Header({ planetNames, planetColors }) {
+  const [navState, setNavState] = useState(false);
+  const navEl = useRef(null);
+
+  function toggleNav() {
+    const nav = navEl.current;
+
+    if (!navState) {
+      nav.classList.remove(classes.hideNav);
+      setNavState(true);
+      return;
+    }
+
+    if (navState) {
+      nav.classList.add(classes.hideNav);
+      setNavState(false);
+      return;
+    }
+  }
+
   return (
     <header className={classes.header}>
-      <Link className={classes.logoLink} href="/">
+      <Link
+        className={classes.logoLink}
+        href="/"
+        onClick={() => {
+          navState ? toggleNav() : "";
+        }}
+      >
         The Planets
       </Link>
 
-      <button className={classes.navBtn}>
+      <button className={classes.navBtn} onClick={toggleNav}>
         <Image src="/assets/icon-hamburger.svg" alt="" width={24} height={17} />
       </button>
 
-      <nav className={classes.nav}>
+      <nav className={`${classes.nav} ${classes.hideNav}`} ref={navEl}>
         <ul className={classes.ul}>
           {planetNames.map((planetName, i) => (
             <li key={planetName} className={classes.li}>
               <Link href={planetName.toLowerCase()}>
-                <div style={{ backgroundColor: `${planetColors[i]}` }}></div>
+                <div style={{ backgroundColor: `${planetColors[i]}` }} />
                 <span>{planetName}</span>
-                <Image src="/assets/chevron.svg" alt="" width={4} height={8} />
+                <Image src="/assets/chevron.svg" alt="" width={6} height={8} />
               </Link>
             </li>
           ))}
--- src/pages/[planet].js
+++ src/pages/[planet].js
@@ -1,20 +1,43 @@
 import { fetchData } from "@/lib/fetch-data";
 
-function PlanetPage({ data }) {
+function PlanetPage({ planet }) {
+  const { name, rotation, revolution, radius, temperature } = planet;
+
+  const overviewContent = planet.overview.content;
+  const overciewSource = planet.overview.source;
+
+  const structureContent = planet.structure.content;
+  const structureSource = planet.structure.source;
+
+  const geologyContent = planet.geology.content;
+  const geologySource = planet.geology.source;
+
+  const imagePlanet = planet.images.planet;
+  const imageInternal = planet.images.internal;
+  const imageGeology = planet.images.geology;
+
   return (
     <>
-      <p>{data[0].name}</p>
+      <p>{name}</p>
+      <p>{overviewContent}</p>
     </>
   );
 }
 
 export default PlanetPage;
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const data = await fetchData();
 
+  const urlSegment = context.params.planet;
+  const planet = data.find((planet) => {
+    if (urlSegment === planet.name.toLowerCase()) {
+      return planet;
+    }
+  });
+
   return {
-    props: { data },
+    props: { planet },
   };
 }
 
